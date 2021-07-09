71cbaf03e139f2cd47ebb279a9b25aaff643d503
Changxiaoying <331980235@qq.com>
2021-07-09 10:29:32 +0800

    AdminController 报错解决

--- src/main/java/org/csu/carecenter/Controller/AdminController.java
+++ src/main/java/org/csu/carecenter/Controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.WebParam;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
