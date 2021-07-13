107982a4fcfcee712bc1fe63f5abc091f396d2c6
Changxiaoying <331980235@qq.com>
2021-07-13 15:06:17 +0800

    客服功能

--- src/main/java/org/csu/carecenter/Controller/MainWebController.java
+++ src/main/java/org/csu/carecenter/Controller/MainWebController.java
@@ -1,18 +1,22 @@
 package org.csu.carecenter.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/main")
 public class MainWebController {
 
-    //跳转到管理员登陆界面
     @GetMapping("/mainWeb")
-    public String signonForm(Model model){
+    public String viewMain()
+    {
         return "mainWeb/index";
+    }
+
+    @GetMapping("/chatRoom")
+    public String chatRoom()
+    {
+        return "websocket";
     }
 }
---
+++ src/main/java/org/csu/carecenter/Controller/WebSocketController.java
@@ -0,0 +1,40 @@
+package org.csu.carecenter.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@SessionAttributes("web")
+public class WebSocketController {
+
+    @RequestMapping("/view")
+    public String view(@RequestParam("username") String name, Model model) {
+        System.out.println(name);
+        log.info("跳转到websocket的页面上");
+        String url = "redirect:/websocket/"+name;
+        System.out.println(url);
+        return url;
+
+    }
+
+    @RequestMapping("/websocket/{username}")
+    public String webSocket(@PathVariable String username, Model model) {
+        try {
+            log.info("跳转到websocket/username的页面上");
+            model.addAttribute("username", username);
+            return "websocket";
+        } catch (Exception e) {
+            log.info("跳转到websocket的页面上发生异常，异常信息是：" + e.getMessage());
+            return "error";
+        }
+    }
+}
