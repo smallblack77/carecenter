7c0d1a44916a855622a8c8bfd950502ed0a61b20
XiEyi1999 <1223508705@qq.com>
2021-07-19 01:27:41 +0800

    膳食关系 bug

--- src/main/java/org/csu/carecenter/Controller/DietController.java
+++ src/main/java/org/csu/carecenter/Controller/DietController.java
@@ -27,6 +27,8 @@ import java.util.List;
 @SessionAttributes("diet")
 public class DietController {
 
+    boolean flag = true;
+
     @Autowired
     private DietService dietService;
 
@@ -184,108 +186,116 @@ public class DietController {
      //   System.out.println(formatter.format(date));
         if(!custId.equals("") && !bre.equals("") && !lunch.equals("") && !dinner.equals("")){
 
-            Customer customer = customerService.getCustomer(Integer.valueOf(custId));
-            if (customer == null){
-                String msg = "该客户不存在";
+            if (flag == true){
+
+                flag= false;
+                Customer customer = customerService.getCustomer(Integer.valueOf(custId));
+                if (customer == null){
+                    String msg = "该客户不存在";
+                    model.addAttribute("msg",msg);
+                    return "dietManage/dietCalendar";
+                }
+
+                OrderDiet orderDiet = new OrderDiet();
+                orderDiet.setDay(currengTime);
+                orderDiet.setCustomerId(custId);
+                orderDiet.setBreakfastId(bre);
+                orderDiet.setDeleteStatus("1");
+                orderDiet.setLunchId(lunch);
+                orderDiet.setDinnerId(dinner);
+
+                dietService.addOrderDiet(orderDiet);
+
+                String msg = "添加成功";
                 model.addAttribute("msg",msg);
+
+                //添加时间线记录
+                //早餐
+                Calendar calendar=Calendar.getInstance();
+                calendar.set(2015, 10, 12,7,30,0);
+                Date breTime = calendar.getTime();
+
+                SimpleDateFormat df1 = new SimpleDateFormat("HH:mm:ss");
+                TimeLine timeLine = new TimeLine();
+                timeLine.setDay(currengTime);
+                timeLine.setDate(df1.format(breTime));
+                timeLine.setCustId(Integer.parseInt(custId));
+
+                Diet diet = dietService.getDietById(Integer.parseInt(bre));
+                String breContent = "早餐时间：" + diet.getName() + "," + diet.getFood1();
+                if(diet.getFood2() != null){
+                    breContent = breContent + "," + diet.getFood2();
+                }
+                if(diet.getFood3() != null){
+                    breContent = breContent + "," + diet.getFood3();
+                }
+                if(diet.getFood4() != null){
+                    breContent = breContent + "," + diet.getFood4();
+                }
+                if(diet.getFood5() != null){
+                    breContent = breContent + "," + diet.getFood5();
+                }
+                timeLine.setContent(breContent);
+
+                customerService.insertTimeLine(timeLine);
+
+                //中餐
+                calendar.set(2015, 10, 12,12,0,0);
+                Date lunchTime = calendar.getTime();
+
+                TimeLine timeLine1 = new TimeLine();
+                timeLine1.setDay(currengTime);
+                timeLine1.setDate(df1.format(lunchTime));
+                timeLine1.setCustId(Integer.parseInt(custId));
+
+                Diet diet1 = dietService.getDietById(Integer.parseInt(lunch));
+                String lunchContent = "午餐时间：" + diet1.getName() + "," + diet1.getFood1();
+                if(diet1.getFood2() != null){
+                    lunchContent = lunchContent + "," + diet1.getFood2();
+                }
+                if(diet1.getFood3() != null){
+                    lunchContent = lunchContent + "," + diet1.getFood3();
+                }
+                if(diet1.getFood4() != null){
+                    lunchContent = lunchContent + "," + diet1.getFood4();
+                }
+                if(diet1.getFood5() != null){
+                    lunchContent = lunchContent + "," + diet1.getFood5();
+                }
+                timeLine1.setContent(lunchContent);
+                customerService.insertTimeLine(timeLine1);
+
+                //晚餐
+                calendar.set(2015, 10, 12,17,30,0);
+                Date dinnerTime = calendar.getTime();
+                TimeLine timeLine2 = new TimeLine();
+                timeLine2.setDay(currengTime);
+                timeLine2.setDate(df1.format(dinnerTime));
+                timeLine2.setCustId(Integer.parseInt(custId));
+
+                Diet diet2 = dietService.getDietById(Integer.parseInt(dinner));
+                String dinnerContent = "晚餐时间：" + diet2.getName() + "," + diet2.getFood1();
+                if(diet2.getFood2() != null){
+                    dinnerContent = dinnerContent + "," + diet2.getFood2();
+                }
+                if(diet2.getFood3() != null){
+                    dinnerContent = dinnerContent + "," + diet2.getFood3();
+                }
+                if(diet2.getFood4() != null){
+                    dinnerContent = dinnerContent + "," + diet2.getFood4();
+                }
+                if(diet2.getFood5() != null){
+                    dinnerContent = dinnerContent + "," + diet2.getFood5();
+                }
+                timeLine2.setContent(dinnerContent);
+                customerService.insertTimeLine(timeLine2);
+
+                return "dietManage/dietCalendar";
+            }else {
+                flag = true;
                 return "dietManage/dietCalendar";
             }
 
-            OrderDiet orderDiet = new OrderDiet();
-            orderDiet.setDay(currengTime);
-            orderDiet.setCustomerId(custId);
-            orderDiet.setBreakfastId(bre);
-            orderDiet.setDeleteStatus("1");
-            orderDiet.setLunchId(lunch);
-            orderDiet.setDinnerId(dinner);
-
-            dietService.addOrderDiet(orderDiet);
-
-            String msg = "添加成功";
-            model.addAttribute("msg",msg);
-
-            //添加时间线记录
-            //早餐
-            Calendar calendar=Calendar.getInstance();
-            calendar.set(2015, 10, 12,7,30,0);
-            Date breTime = calendar.getTime();
-
-            SimpleDateFormat df1 = new SimpleDateFormat("HH:mm:ss");
-            TimeLine timeLine = new TimeLine();
-            timeLine.setDay(currengTime);
-            timeLine.setDate(df1.format(breTime));
-            timeLine.setCustId(Integer.parseInt(custId));
-
-            Diet diet = dietService.getDietById(Integer.parseInt(bre));
-            String breContent = "早餐时间：" + diet.getName() + "," + diet.getFood1();
-            if(diet.getFood2() != null){
-                breContent = breContent + "," + diet.getFood2();
-            }
-            if(diet.getFood3() != null){
-                breContent = breContent + "," + diet.getFood3();
-            }
-            if(diet.getFood4() != null){
-                breContent = breContent + "," + diet.getFood4();
-            }
-            if(diet.getFood5() != null){
-                breContent = breContent + "," + diet.getFood5();
-            }
-            timeLine.setContent(breContent);
-
-            customerService.insertTimeLine(timeLine);
-
-            //中餐
-            calendar.set(2015, 10, 12,12,0,0);
-            Date lunchTime = calendar.getTime();
-
-            TimeLine timeLine1 = new TimeLine();
-            timeLine1.setDay(currengTime);
-            timeLine1.setDate(df1.format(lunchTime));
-            timeLine1.setCustId(Integer.parseInt(custId));
-
-            Diet diet1 = dietService.getDietById(Integer.parseInt(lunch));
-            String lunchContent = "午餐时间：" + diet1.getName() + "," + diet1.getFood1();
-            if(diet1.getFood2() != null){
-                lunchContent = lunchContent + "," + diet1.getFood2();
-            }
-            if(diet1.getFood3() != null){
-                lunchContent = lunchContent + "," + diet1.getFood3();
-            }
-            if(diet1.getFood4() != null){
-                lunchContent = lunchContent + "," + diet1.getFood4();
-            }
-            if(diet1.getFood5() != null){
-                lunchContent = lunchContent + "," + diet1.getFood5();
-            }
-            timeLine1.setContent(lunchContent);
-            customerService.insertTimeLine(timeLine1);
-
-            //晚餐
-            calendar.set(2015, 10, 12,17,30,0);
-            Date dinnerTime = calendar.getTime();
-            TimeLine timeLine2 = new TimeLine();
-            timeLine2.setDay(currengTime);
-            timeLine2.setDate(df1.format(dinnerTime));
-            timeLine2.setCustId(Integer.parseInt(custId));
-
-            Diet diet2 = dietService.getDietById(Integer.parseInt(dinner));
-            String dinnerContent = "晚餐时间：" + diet2.getName() + "," + diet2.getFood1();
-            if(diet2.getFood2() != null){
-                dinnerContent = dinnerContent + "," + diet2.getFood2();
-            }
-            if(diet2.getFood3() != null){
-                dinnerContent = dinnerContent + "," + diet2.getFood3();
-            }
-            if(diet2.getFood4() != null){
-                dinnerContent = dinnerContent + "," + diet2.getFood4();
-            }
-            if(diet2.getFood5() != null){
-                dinnerContent = dinnerContent + "," + diet2.getFood5();
-            }
-            timeLine2.setContent(dinnerContent);
-            customerService.insertTimeLine(timeLine2);
-
-            return "account/index";
         }else {
             String msg = "输入不能为空";
             model.addAttribute("msg",msg);
